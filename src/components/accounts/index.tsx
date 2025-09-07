c6d51461c1749873ea88d78fb8313c13c785cd4c
Jack Hewitt <hewittjack2011@gmail.com>
2025-09-07 20:16:52 +0100

    Task02: Add loading handler for fetching data

--- src/components/accounts/index.tsx
+++ src/components/accounts/index.tsx
@@ -1,18 +1,23 @@
 import { useAccounts } from "../../api/hooks";
+import { Loading } from "../loading";
 import { AccountItem } from "./item";
 import "./index.css";
 
 export const Accounts = () => {
-  const { accounts } = useAccounts();
+  const { accounts, loading } = useAccounts();
 
   return (
     <>
       <h1 className="align-left">Your accounts</h1>
+      {loading ? (
+        <Loading />
+      ) : (
       <div className="accounts">
         {accounts.map((account) => (
           <AccountItem account={account} key={account.account_id} />
         ))}
       </div>
+      )}
     </>
   );
 };
--- src/components/transactions/index.tsx
+++ src/components/transactions/index.tsx
@@ -1,8 +1,10 @@
 import { useTransactions } from "../../api/hooks";
+import { Loading } from "../loading";
 import * as Tabs from "@radix-ui/react-tabs";
 import { Transaction as TransactionType } from "../../../types";
 import "./index.css";
 import { Transaction } from "./item";
+import React from "react";
 
 const isExpense = (transaction: TransactionType) => transaction.amount.value < 0;
 const isIncome = (transaction: TransactionType) => transaction.amount.value > 0;
@@ -49,23 +51,29 @@ const Income = ({transactions}: TableProps) => {
 };
 
 export const TransactionHistory = () => {
-  const { transactions } = useTransactions();
+  const { transactions, loading } = useTransactions();
 
   return (
     <>
       <h1 className="align-left">Transaction History</h1>
+
       <Tabs.Root defaultValue="expenses" className="flow">
         <Tabs.List className="tabs__list" aria-label="Filter your transactions">
           <Tabs.Trigger value="expenses">Expenses</Tabs.Trigger>
           <Tabs.Trigger value="income">Income</Tabs.Trigger>
         </Tabs.List>
-
-        <Tabs.Content className="TabsContent" value="expenses">
-          <Expenses transactions={transactions} />
-        </Tabs.Content>
-        <Tabs.Content className="TabsContent" value="income">
-          <Income transactions={transactions} />
-        </Tabs.Content>
+        {loading ? (
+        <Loading />
+      ) : (
+        <React.Fragment> 
+          <Tabs.Content className="TabsContent" value="expenses">
+            <Expenses transactions={transactions} />
+          </Tabs.Content>
+          <Tabs.Content className="TabsContent" value="income">
+            <Income transactions={transactions} />
+          </Tabs.Content>
+        </React.Fragment>
+        )}
       </Tabs.Root>
     </>
   );
