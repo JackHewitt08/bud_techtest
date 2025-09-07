66e7ce7bf13160141e1156b472622933165fb785
Jack Hewitt <35602668+JackHewitt08@users.noreply.github.com>
2025-09-07 17:21:55 +0100

    Initial commit

---
+++ types/index.ts
@@ -0,0 +1,24 @@
+type Balance = {
+  amount: {
+    currency: string
+    value: number
+  }
+};
+
+export type Transaction = {
+  id: string;
+  account_id: string;
+  date: string;
+  description: string;
+  category: string;
+  amount: {
+    value: number;
+    currency_iso: string;
+  };
+};
+
+export type Account = {
+  account_id: string;
+  balance: Balance;
+  display_name: string;
+};
